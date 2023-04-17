84d4277e2eb591c025bedb00aff76240158e5997
KIM <2016124068@kau.kr>
2023-04-17 17:11:45 +0200

    5th commit: Setting static configuration & CSS codes from styles to Classes in base.css

--- berryland/settings.py
+++ berryland/settings.py
@@ -133,10 +133,24 @@ USE_I18N = True
 USE_TZ = True
 
 
+#############################################################
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# By using 'collectstatic' you can collect every static files
+# to one spot. The spot is indicated by 'STATIC_ROOT'.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Can indicate a folder where static files are saved which is
+# independent to each APPs.
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+#############################################################
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
