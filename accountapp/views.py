299b7bb03edc17520e086f10b8045773fa163f98
KIM <2016124068@kau.kr>
2023-04-17 21:50:28 +0200

    8th commit: Saving data into DB using POST method

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import Hello
 
 
 def hello(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello.html', context={'text': 'POST METHOD!'})
+
+        temp = request.POST.get('hello_input')
+
+        new_hello = Hello()
+        new_hello.text = temp
+        new_hello.save()
+
+        return render(request, 'accountapp/hello.html', context={'hello_output': new_hello})
+
     else:
         return render(request, 'accountapp/hello.html', context={'text': 'GET METHOD!'})
 
