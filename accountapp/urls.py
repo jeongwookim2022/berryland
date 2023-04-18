e5732237f5d5f9852c9ed3c11714f638adb0846b
KIM <2016124068@kau.kr>
2023-04-18 14:07:40 +0200

    13th commit: Implemented MyPage using DetailView & Context_object_name(user-> target_user)

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello, AccountCreateView
+from accountapp.views import hello, AccountCreateView, AccountDetailView
 
 app_name = "accountapp"  # "127.0.0.1:8000/account/hello" -> "accountapp:hello"
 
@@ -12,6 +12,9 @@ urlpatterns = [
     # For LoginView, indicating 'template_name' is required.
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    # Detail(Read)
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 
 
 ]
