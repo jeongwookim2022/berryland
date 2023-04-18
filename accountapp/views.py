83c078cba1fda53980896cc2a9f423faf08d4275
KIM <2016124068@kau.kr>
2023-04-18 17:23:33 +0200

    17th commit: Implemented Authentication system for users' Update & Delete page from other users By using is_authenticated & get_object() in View Classes and HttpResponseForbidden().

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
@@ -12,28 +12,35 @@ from accountapp.models import Hello
 
 
 def hello(request):
-    if request.method == "POST":
+    # Authentication
+    if request.user.is_authenticated:
+        if request.method == "POST":
 
-        temp = request.POST.get('hello_input')
+            temp = request.POST.get('hello_input')
 
-        new_hello = Hello()
-        new_hello.text = temp
-        new_hello.save()
+            new_hello = Hello()
+            new_hello.text = temp
+            new_hello.save()
 
-        # hello_list = Hello.objects.all()
+            # hello_list = Hello.objects.all()
 
-        # ###After responding POST request, Redirect to current URL 'account/hello/'.###
-        # HttpResponseRedirect(reverse()) -> For not writing full URL.
-        # By doing so above, it's redirected the current URL.
-        # That means it's GET request then -> it renders 'hello.html' and sends 'hello_list'
-        # As a result, it shows every 'hello' in 'hello_list'.
+            # ###After responding POST request, Redirect to current URL 'account/hello/'.###
+            # HttpResponseRedirect(reverse()) -> For not writing full URL.
+            # By doing so above, it's redirected the current URL.
+            # That means it's GET request then -> it renders 'hello.html' and sends 'hello_list'
+            # As a result, it shows every 'hello' in 'hello_list'.
 
-        # return render(request, 'accountapp/hello.html', context={'hello_list': hello_list})
-        return HttpResponseRedirect(reverse('accountapp:hello'))
+            # return render(request, 'accountapp/hello.html', context={'hello_list': hello_list})
+            return HttpResponseRedirect(reverse('accountapp:hello'))
 
+        else:
+            hello_list = Hello.objects.all()
+            return render(request, 'accountapp/hello.html', context={'hello_list': hello_list})
+
+    # Not Authenticated
+    # -> Redirect to Login page
     else:
-        hello_list = Hello.objects.all()
-        return render(request, 'accountapp/hello.html', context={'hello_list': hello_list})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 # ################################## Class Based View ########################################
@@ -60,9 +67,39 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello')  # It's CBV so using 'reverse()' throws an error.
     template_name = 'accountapp/update.html'
 
+    # Adjusted GET &POST methods to prevent UnAuthenticated user from viewing or updating his/her Info After logged out.
+    # But he/she can still update/delete other people's info.
+    # -> By using 'get_object()', can get 'User object' who is the current PK.
+    #    And check if the current 'requesting user' is the same 'pk user'.
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            # return HttpResponseRedirect(reverse('accountapp:login'))
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().post(*args, **kwargs)
+        else:
+            # return HttpResponseRedirect(reverse('accountapp:login'))
+            return HttpResponseForbidden()
+
 
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().post(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
