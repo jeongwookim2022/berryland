e4be5aecd8b27493fe4dbd14320577f689b52fe0
KIM <2016124068@kau.kr>
2023-04-19 22:02:21 +0200

    23th commit: Added 'get_success_url(self)' in profile/views.py for natural Redirection and Fixed some not natural parts in account/detail.html for profile.

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,7 +14,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello')
+    # success_url = reverse_lazy('accountapp:hello')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -32,6 +32,13 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    # 1. success_url = reverse_lazy('accountapp:hello')
+    # - After creating or updating a profile, Redirecting to 'Detail page' is more natural.
+    # 2. self.object
+    # - 'object' here is 'profile'.
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -39,5 +46,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello')
+    # success_url = reverse_lazy('accountapp:hello')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
