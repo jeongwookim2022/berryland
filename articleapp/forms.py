bb0a9f741f0f790e3cee47c5d472440b31b5c240
KIM <2016124068@kau.kr>
2023-04-21 22:13:07 +0200

    35th commit: Applied WYSIWIG when creating&updating an Article and Allowed users to not have to choose a project when creating&updating an Article.

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
@@ -7,7 +8,17 @@ from articleapp.models import Article
 # - Will set in server area. By using 'def form_valid(self)' in profileapp/views.py.
 
 # 2. Added 'project' in fields
+# 3. Added 'WYSIWIG'
+from projectapp.models import Project
+
+
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height: auto;'}))
+
+    # It allows users to not choose a project when creating an article.
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=True)
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
